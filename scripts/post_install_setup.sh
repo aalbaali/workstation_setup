a1695296ac134c16acf8444ee56542ede480c5c7
aalbaali <albaalia@live.com>
2022-08-26 21:20:05 -0400

    Redirect error stream
    Signed-off-by: aalbaali <albaalia@live.com>

--- scripts/post_install_setup.sh
+++ scripts/post_install_setup.sh
@@ -246,5 +246,5 @@ esac
 
 # Zsh zplug
 if [ $INSTALL_ALL ] || [ "${STOW_PACKAGES[zsh]}" = true ]; then
-  zsh <<< 'ZSH_NONINTERACTIVE=true; source ~/.zshrc; zplug install'
+  zsh <<< 'ZSH_NONINTERACTIVE=true; source ~/.zshrc; zplug install' 2>&1 >/dev/null
 fi
