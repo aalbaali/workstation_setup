a0936f1dd5dee9989977ed105cbf5da5ffe04066
aalbaali <albaalia@live.com>
2023-03-23 22:48:53 -0400

    Add another lsp config
    Signed-off-by: aalbaali <albaalia@live.com>

---
+++ stow/nvim/.config/nvim/lua/user/lsp2/init.lua
@@ -0,0 +1,145 @@
+local lsp = require('lsp-zero')
+
+require "user.lsp.mason"
+require("user.lsp.handlers").setup()
+require "user.lsp.null-ls"
+
+lsp.preset({
+  name = 'minimal',
+  set_lsp_keymaps = true,
+  manage_nvim_cmp = true,
+  suggest_lsp_servers = false,
+})
+
+local servers = {
+	-- "xml",
+	-- "html",
+	-- "tsserver",
+	"pyright",
+	"jsonls",
+  "clangd",
+	"lua_ls",
+}
+lsp.ensure_installed(servers)
+
+
+-- Fix Undefined global 'vim'
+lsp.configure('lua_ls', {
+    settings = {
+        Lua = {
+            diagnostics = {
+                globals = { 'vim' }
+            }
+        }
+    }
+})
+
+-- local cmp = require('cmp')
+-- local cmp_select = {behavior = cmp.SelectBehavior.Select}
+-- local cmp_mappings = lsp.defaults.cmp_mappings({
+--   ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+--   ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+--   ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+--   ["<C-Space>"] = cmp.mapping.complete(),
+-- })
+--
+
+-- cmp_mappings['<Tab>'] = nil
+-- cmp_mappings['<S-Tab>'] = nil
+
+--lsp.setup_nvim_cmp({
+--  mapping = cmp_mappings
+--})
+
+-- local cmp = require("user.cmp")
+-- lsp.setup_nvim_cmp(cmp)
+--
+lsp.set_preferences({
+    suggest_lsp_servers = true,
+    sign_icons = {
+        error = "",
+        warn =  "",
+        hint =  "",
+        info =  ""
+    }
+})
+
+-- lsp.start('clangd', {
+--   cmd = { 'clangd', '--background-index', '--clang-tidy', '--completion-style=detailed', '--header-insertion=iwyu', '--suggest-missing-includes', '-j=4' },
+--   init_options = {
+--     clangdFileStatus = true,
+--     usePlaceholders = true,
+--     completeUnimported = true,
+--     semanticHighlighting = true,
+--     includePaths = {
+--       '/usr/include',
+--       '/usr/local/include',
+--     },
+--   },
+-- })
+--
+local function lsp_keymaps(bufnr)
+	local opts = { noremap = true, silent = true }
+	local keymap = vim.api.nvim_buf_set_keymap
+	keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+	keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+	keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+	keymap(bufnr, "n", "gI", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+	keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+	keymap(bufnr, "n", "gl", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
+	keymap(bufnr, "n", "<leader>lf", "<cmd>lua vim.lsp.buf.format{ async = true }<cr>", opts)
+	keymap(bufnr, "n", "<leader>li", "<cmd>LspInfo<cr>", opts)
+	keymap(bufnr, "n", "<leader>lI", "<cmd>LspInstallInfo<cr>", opts)
+	keymap(bufnr, "n", "<leader>la", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
+	keymap(bufnr, "n", "]g", "<cmd>lua vim.diagnostic.goto_next({buffer=0})<cr>", opts)
+	keymap(bufnr, "n", "[g", "<cmd>lua vim.diagnostic.goto_prev({buffer=0})<cr>", opts)
+	keymap(bufnr, "n", "<leader>lr", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
+	keymap(bufnr, "n", "<leader>ls", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+	keymap(bufnr, "n", "<leader>lq", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
+end
+
+lsp.on_attach = function(client, bufnr)
+  print("HII")
+	if client.name == "tsserver" then
+		client.server_capabilities.documentFormattingProvider = false
+	end
+
+	if client.name == "lua_ls" then
+		client.server_capabilities.documentFormattingProvider = false
+	end
+
+	if client.name == "clangd" then
+    print("Finished adding settings")
+    -- -- set additional options for this LSP server
+    -- client.resolved_capabilities.document_formatting = false
+    -- client.resolved_capabilities.document_range_formatting = false
+    --
+    -- -- set include paths for this LSP server
+    -- client.resolved_capabilities.initialization_options = {
+    --   clangdFileStatus = true,
+    --   usePlaceholders = true,
+    --   completeUnimported = true,
+    --   semanticHighlighting = true,
+    --   includePaths = {
+    --     '/usr/include',
+    --     '/usr/local/include',
+    --   },
+    -- }
+    --
+  end
+
+	lsp_keymaps(bufnr)
+	local status_ok, illuminate = pcall(require, "illuminate")
+	if not status_ok then
+    print("NOOO")
+		return
+	end
+	illuminate.on_attach(client)
+end
+
+lsp.setup()
+
+vim.diagnostic.config({
+    virtual_text = false
+})
+
