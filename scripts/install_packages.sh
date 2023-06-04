2ddf95a81abbb89dd8a18d50be318dc8ecfd028d
aalbaali <albaalia@live.com>
2023-06-03 23:05:50 -0400

    Add nvim installation instructions
    Signed-off-by: aalbaali <albaalia@live.com>

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -67,10 +67,21 @@ then
     echo "Symbolic link created from bat to batcat"
 fi
 
-## install latest nvim release
-#sudo add-apt-repository -y ppa:neovim-ppa/stable
-#sudo apt-get update
-#sudo apt-get install -y neovim
+# install latest nvim release, if there's no nvim or if it's older than the target version
+_nvim_version="$(nvim --version | grep -oP 'NVIM v\K\d+\.\d+')"
+_nvim_version_major=$(echo $_nvim_version | awk -F'.' '{print $1}')
+_nvim_version_minor=$(echo $_nvim_version | awk -F'.' '{print $2}')
+_nvim_target_version_major=0
+_nvim_target_version_minor=8
+if [[ $_nvim_version_major -lt $_nvim_target_version_major ]] \
+        && [[ $_nvim_version_minor -le _nvim_target_version_minor ]]; then
+  cd /tmp
+  wget https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz
+  tar -xvf nvim-linux64.tar.gz
+  sudo mv /tmp/nvim-linux64 /opt
+  sudo ln -s /opt/nvim-linux64/bin/nvim /usr/bin/nvim -f
+  cd -
+fi
 
 # Install lazygit using golang
 # go install github.com/jesseduffield/lazygit@latest
