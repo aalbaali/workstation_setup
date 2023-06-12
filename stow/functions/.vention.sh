03f7cb87cca31a1705af9fcb8471720f01ee7189
amro-albaali-vention <amro.al-baali@vention.cc>
2023-06-12 11:26:20 -0400

    Add vention-specific ros functions
    Signed-off-by: amro-albaali-vention <amro.al-baali@vention.cc>

--- stow/functions/.vention.sh
+++ stow/functions/.vention.sh
@@ -1,9 +1,27 @@
 # Export vention environment variables
-export TOKEN_FILE="~/vention/TOKEN.txt"
+export TOKEN_PATH="$HOME/vention/TOKEN.txt"
 export GITHUB_TOKEN=$(cat ~/vention/TOKEN.txt)
 
 # Copy token to clipboard
 alias cptoken="cat ~/vention/TOKEN.txt | xclip -selection clipboard"
 
 # Always use local docker env vars
-export DOCKER_MULTI_INSTANCE_RUN_SERVER_ENVIRONMENT=LOCAL
+#export DOCKER_MULTI_INSTANCE_RUN_SERVER_ENVIRONMENT=LOCAL
+
+# Find ROS services
+fdsrv() {
+  rosservice list | grep -i "$1"
+}
+
+srvtype() {
+  local srvname
+  srvname="$(cat -)"
+
+  # Get the service type
+  rosservice info "$srvname" | grep "^Type" | awk -F':' '{print $2}'
+}
+
+# Source asdf (required for vention Rails)
+source "$HOME/.asdf/asdf.sh"
+
+alias jmrbs="cd ~/vention/multirepo-build-system"
