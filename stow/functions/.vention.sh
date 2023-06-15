23c40f8ecc93f829d928ec30dc07f81d1fd78ca5
amro-albaali-vention <amro.al-baali@vention.cc>
2023-06-15 17:25:00 -0400

    Add vention- session creations
    Signed-off-by: amro-albaali-vention <amro.al-baali@vention.cc>

--- stow/functions/.vention.sh
+++ stow/functions/.vention.sh
@@ -25,3 +25,75 @@ srvtype() {
 source "$HOME/.asdf/asdf.sh"
 
 alias jmrbs="cd ~/vention/multirepo-build-system"
+
+function _create_new_tmux_window_and_send_keys() {
+  # Create a new tmux window, change working directory,
+  # and send keys (without executing them)
+  session="$1"
+  window="$2"
+  wd="$3"      # Working directory
+  cmd="$4"     # Command to send
+
+  _create_new_tmux_window $session "$window"
+  [[ -n "$wd" ]] && tmux send-keys -t "$session:$window" "cd $wd" Enter
+  [[ -n "$cmd" ]] && tmux send-keys -t "$session:$window" "$cmd"
+}
+
+# Default session name
+DEFAULT_VENTION_SESSION_NAME="ven-run"
+
+function vention_ses_kill() {
+  session="$1"
+  if [[ -z "$session" ]]; then
+    session="$DEFAULT_VENTION_SESSION_NAME"
+    log_info "No session name provided. Using default '$session'"
+  fi
+  if ! _does_tmux_session_exist "$session"; then
+    log_info "Attempting to delete a non-existing session. Ignoring."
+    return 1
+  fi
+  tmux kill-ses -t "$session"
+}
+
+# Run Vention packages in tmux
+function vention_ses_start() {
+  session="$1"
+  should_kill="$2"
+  if [[ -z "$session" ]]; then
+    session="$DEFAULT_VENTION_SESSION_NAME"
+    log_info "No session name provided. Using default '$session'"
+  fi
+
+  if _does_tmux_session_exist "$session"; then
+    if [[ -n "$should_kill" ]]; then
+      vention_ses_kill "$session"
+    else
+      log_error "Sesssion '$session' already exists. Aborting"
+      return 1
+    fi
+  fi
+  log_info "Session '$session' doesn't exist. It'll be created"
+  first_window=1
+
+  # Rails
+  _create_new_tmux_window_and_send_keys "$session" \
+    "Rails" \
+    "~/vention/multirepo-build-system/vention_rails" \
+    "npm run start:vention-rails-run"
+
+  # Assembler
+  _create_new_tmux_window_and_send_keys "$session" \
+    "Asmblr" \
+    "~/vention/multirepo-build-system/vention_assembler/client" \
+    "npm run dev"
+}
+
+function vention_ses_restart() {
+  session="$1"
+  if [[ -z "$session" ]]; then
+    session="$DEFAULT_VENTION_SESSION_NAME"
+    log_info "No session name provided. Using default '$session'"
+  fi
+  vention_ses_kill $session
+  vention_ses_start "$session"
+}
