24987331df71c53bbd189c32d1ad597106509c48
aalbaali <albaalia@live.com>
2023-08-23 11:35:00 -0400

    Simplify if-else statement in installing node
    Signed-off-by: aalbaali <albaalia@live.com>

--- scripts/install_node.sh
+++ scripts/install_node.sh
@@ -6,7 +6,12 @@
 ! command -v node &> /dev/null && _should_update_node=1
 
 # Check node version
-if [[ -z "$_should_update_node" ]]; then
+if [[ -n "$_should_update_node" ]]; then
+  echo "Installing nodejs version 16"
+  curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+  sudo apt-get update -y
+  sudo apt-get install -y nodejs
+else
   target_version=16
   # Check if node version is under 16
   if [[ $(node -v | cut -d "." -f 1 | cut -d "v" -f 2) -lt $target_version ]]; then
