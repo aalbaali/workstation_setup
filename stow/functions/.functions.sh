4276a485407a666c81809f3a28ec81bdb610cbd5
aalbaali <albaalia@live.com>
2023-08-31 16:47:39 -0400

    Update jfranklin alias and starship config
    Signed-off-by: aalbaali <albaalia@live.com>

--- stow/functions/.functions.sh
+++ stow/functions/.functions.sh
@@ -17,7 +17,7 @@ alias agn="ag -l"
 alias rgh="rg --hidden"
 alias vsc="code -n ."
 alias jj=julia
-alias jfranklin="julia --project=Project.toml -e 'using Franklin; serve()'"
+alias jfranklin="julia --project=Project.toml -e 'using Pkg; Pkg.instantiate(); using Franklin; serve()'"
 alias jpluto="julia -e 'using Pluto; Pluto.run()'"
 #alias vpdf=sioyek
 alias vpdf=evince            # Pdf viewer
