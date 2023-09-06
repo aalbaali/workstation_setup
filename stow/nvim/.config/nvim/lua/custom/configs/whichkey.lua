9ff0d4bceea7b81a64690d00cd5f12e3df87f4db
aalbaali <albaalia@live.com>
2023-09-06 17:41:50 -0400

    Use ww to save doc
    Signed-off-by: aalbaali <albaalia@live.com>

--- stow/nvim/.config/nvim/lua/custom/configs/whichkey.lua
+++ stow/nvim/.config/nvim/lua/custom/configs/whichkey.lua
@@ -85,7 +85,7 @@ local mappings = {
     "Buffers",
   },
   ["e"] = { "<cmd>NvimTreeToggle<cr>", "Toggle explorer" },
-  ["w"] = { "<cmd>w!<CR>", "Save" },
+  ["ww"] = { "<cmd>w!<CR>", "Save" },
   ["X"] = { "<cmd>q!<CR>", "Quit without save" },
   ["q"] = { "<cmd>wqa<CR>", "Save all and quit" },
   ["c"] = { "<cmd>Bdelete!<CR>", "Close Buffer" },
