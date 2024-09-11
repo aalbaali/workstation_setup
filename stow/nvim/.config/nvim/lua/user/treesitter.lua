a7a8bc2395d10ba5f6c734a7c7b00f2bfaac14fe
amro <amro@picklerobot.com>
2024-09-11 10:23:02 -0400

    Update Treesitter installed configs
    Signed-off-by: amro <amro@picklerobot.com>

--- stow/nvim/.config/nvim/lua/user/treesitter.lua
+++ stow/nvim/.config/nvim/lua/user/treesitter.lua
@@ -4,7 +4,22 @@ if not status_ok then
 end
 
 configs.setup({
-  ensure_installed = { "lua", "bash", "c", "cpp", "json", "lua", "python", "css", "yaml", "markdown", "markdown_inline" }, -- one of "all" or a list of languages
+	ensure_installed = {
+		"lua",
+		"bash",
+		"c",
+		"cpp",
+		"json",
+		"lua",
+		"python",
+		"css",
+		"yaml",
+		"markdown",
+		"markdown_inline",
+		"vimdoc",
+		"luadoc",
+		"vim",
+	}, -- one of "all" or a list of languages
 	ignore_install = { "phpdoc" }, -- List of parsers to ignore installing
 	highlight = {
 		enable = true, -- false will disable the whole extension
