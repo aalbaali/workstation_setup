287936afda4a74be70dabe16b8543bd51d5d428b
amro <albaalia@live.com>
2026-01-03 23:24:00 -0500

    Upgrade indent blankline

--- stow/nvim/.config/nvim/lua/user/plugins.lua
+++ stow/nvim/.config/nvim/lua/user/plugins.lua
@@ -53,7 +53,7 @@ return packer.startup(function(use)
   use { "akinsho/toggleterm.nvim" }
   --use { "ahmedkhalf/project.nvim" }
   use { "lewis6991/impatient.nvim" }
-  use { "lukas-reineke/indent-blankline.nvim", commit = 'v2.20.8' }
+  use { "lukas-reineke/indent-blankline.nvim", commit = 'v3.9.0' }
   -- use { "goolord/alpha-nvim" }
   use { "folke/which-key.nvim", commit = "v2.1.0" }
 
